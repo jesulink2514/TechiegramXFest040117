f191e87c5aa1596c5fa8523cb3342d8d31f533f0
Jesus Angulo <jesus.angulo@outlook.com>
2017-03-24 13:08:48 -0500

    Add project files.

---
+++ Techiegram/Techiegram/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Techiegram.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
